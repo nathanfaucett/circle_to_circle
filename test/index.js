7c2760591e26a5bac0148906a56c77be48e8f2ca
nathanfaucett <nathanfaucett@gmail.com>
2015-10-30 17:01:36 -0500

    updates

--- test/index.js
+++ test/index.js
@@ -1,17 +1,12 @@
 var tape = require("tape"),
+    vec2 = require("vec2"),
     circleToCircle = require("..");
 
 
 tape("circleToCircle(x1 : Number, y1 : Number, r1 : Number, x2 : Number, y2 : Number, r2 : Number)", function(assert) {
     assert.deepEqual(circleToCircle(-1, 0, 1.1, 1, 0, 1.1), {
-        normal: {
-            0: 1,
-            1: 0
-        },
-        point: {
-            0: 0,
-            1: 0
-        },
+        normal: vec2.create(1, 0),
+        point: vec2.create(0, 0),
         depth: 0.10000000000000009,
         data: null
     });
